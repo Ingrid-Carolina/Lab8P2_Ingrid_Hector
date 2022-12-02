a973ca28635f08d467f2a2bbde6226869db5b352
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-12-02 13:37:15 -0600

    first

---
+++ src/lab8p2_ingridhernandez_hectorhernandez/Lab8P2_IngridHernandez_HectorHernandez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab8p2_ingridhernandez_hectorhernandez;
+
+/**
+ *
+ * @author ADMINISTRADOR1
+ */
+public class Lab8P2_IngridHernandez_HectorHernandez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
